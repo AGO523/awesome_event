ededebfd8cd482dbcbc596220d9cb6a85dc73f65
AGO523 <unpouletfrais0523@gmail.com>
2022-03-09 06:11:21 +0900

    add: signin pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  def create_table
+  def create
     user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
-    session[:user_id] = user.index
+    session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
   end
 end
