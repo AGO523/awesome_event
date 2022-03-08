63c9034fc4420aac13fbc6077ed5ebe4cc145cf2
AGO523 <unpouletfrais0523@gmail.com>
2022-03-09 06:21:48 +0900

    add: logout action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?
+
+  private
+
+  # logged_in?メソッドでは、session[:user_id]に値が入っていればログインとしてtrue、そうでなければ未ログインとしてfalseを返しています。!!session[:user_id]の先頭に付いている2つの!はnot演算子です。not演算子を2つ重ねることで、session[:user_id]がfalseまたはnilの時はfalseを、それ以外の値の時はtrueへ変換します。
+  def logged_in?
+    !!session[:user_id]
+  end
 end
