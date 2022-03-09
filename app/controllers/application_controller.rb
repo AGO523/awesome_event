8ccb99ba115b76e6e90a190ce1cad80de7ebafc7
AGO523 <unpouletfrais0523@gmail.com>
2022-03-10 05:47:27 +0900

    add: login controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
+  before_action :authenticate
+  helper_method :logged_in?, :current_user
 
   private
 
-  # logged_in?メソッドでは、session[:user_id]に値が入っていればログインとしてtrue、そうでなければ未ログインとしてfalseを返しています。!!session[:user_id]の先頭に付いている2つの!はnot演算子です。not演算子を2つ重ねることで、session[:user_id]がfalseまたはnilの時はfalseを、それ以外の値の時はtrueへ変換します。
+  # logged_in?メソッドでは、session[:user_id]に値が入っていればログインとしてtrue、そうでなければ未ログインとしてfalseを返しています。!!はnot演算子です。not演算子を2つ重ねることで、session[:user_id]がfalseまたはnilの時はfalseを、それ以外の値の時はtrueへ変換します。
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user || = User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
