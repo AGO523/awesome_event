e98be81cfc2da0fdbe1b10fac7fec83a587f8a97
AGO523 <unpouletfrais0523@gmail.com>
2022-03-12 08:55:38 +0900

    add: edit views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,18 @@ before_action :authenticate, only: :show
     end
   end
 
+  def edit 
+    # イベントを作成したユーザだけがイベント編集ページにアクセス可能
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def updated_at
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました"
+    end
+  end
+
   private
 
   def event_params
