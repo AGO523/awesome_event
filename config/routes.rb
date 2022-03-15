7b0d2affbee33ede5012e63c1f6387ed439d644a
AGO523 <unpouletfrais0523@gmail.com>
2022-03-16 08:20:59 +0900

    fix: ルーティングの制限を編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   
-  resources :events do 
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
 
-  resource :retirements
+  resource :retirements, only: %i[new create]
 
   # どのルーティング設定にも当てはまらないリクエストが来た場合に、error404アクションを実行するように設定を追加しました。これにより、適当なURLをブラウザに入力すると「ご指定になったページは存在しません」と表示されるようになりました
   match "*path" => "application#error404", via: :all
