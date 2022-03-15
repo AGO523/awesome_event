9d085ab5131da55c98a91bd46c5697bc4ae710a5
AGO523 <unpouletfrais0523@gmail.com>
2022-03-16 08:18:10 +0900

    add: 開発ようのエラーログ表示の設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # 開発環境で、本番環境用のエラーページを表示させる
+  config.consider_all_requests_local = false
+
   # Do not eager load code on boot.
   config.eager_load = false
 
